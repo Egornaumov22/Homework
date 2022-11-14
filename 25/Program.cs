8675562192452906b3ef7589e5a7c98a765751ad
egor <eegorsky@gmail.com>
2022-11-14 20:10:39 +0300

    hw

---
+++ 25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("введите число");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите степень");
+int num2 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine(num +", " + num2 + " = " + Powr(num, num2));
+
+int Powr(int a, int b)
+{
+    int res =1;
+    for( int i =0;i<b; i++)
+    {
+        res *= a;
+    }
+return res;
+}
+
---
+++ 27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу,
+//  которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+Console.WriteLine("введите число");
+int num = int.Parse(Console.ReadLine());
+
+int Sumchisel(int num1)
+{
+    int sum=0;
+    int i=0;
+while(num>0)
+    {
+        sum=sum+num%10;
+        num/=10;
+    }
+    return sum;
+}
+
+Console.WriteLine("сумма цифр = " + Sumchisel(num));
